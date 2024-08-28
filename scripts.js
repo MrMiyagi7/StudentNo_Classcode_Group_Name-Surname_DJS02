9a4e1a48d5a791cca95debdec9dd087dad1ede6c
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-08-28 17:37:57 +0200

    added try catch and logic to handle empty input

--- scripts.js
+++ scripts.js
@@ -2,8 +2,17 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider);
+  try {
+    event.preventDefault();
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+    const noDataErr =
+      "Division not performed. Both values are required in inputs. Try again";
+    console.log(dividend);
+    console.log(divider);
+    if (dividend === "" || divider === "") throw noDataErr;
+    result.innerText = Math.floor(dividend / divider);
+  } catch (err) {
+    result.innerHTML = err;
+  }
 });
