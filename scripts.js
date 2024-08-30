9b43e5bc5c6c3f4b0b8ec11beb5f3de1a6d596da
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-08-30 13:04:32 +0200

    Added logic for critical error

--- scripts.js
+++ scripts.js
@@ -10,15 +10,30 @@ form.addEventListener("submit", (event) => {
       "Division not performed. Both values are required in inputs. Try again";
     const invalidNumberErr =
       "Division not performed. Invalid number provided. Try again";
-
+    const criticalErr = "Something critical went wrong. Please reload the page";
+    if (isNaN(dividend) || isNaN(divider)) throw criticalErr;
     if (dividend === "" || divider === "") throw noDataErr;
     if (dividend == 0 || divider == 0) throw invalidNumberErr;
+
     result.innerText = Math.floor(dividend / divider);
   } catch (err) {
-    result.innerHTML = err;
     if (err === "Division not performed. Invalid number provided. Try again") {
       console.error(err);
       console.trace();
+
+      result.innerHTML = `<h1 class="error-message">${err}</h1>`;
+    } else if (
+      err === "Something critical went wrong. Please reload the page"
+    ) {
+      document.body.innerHTML = `<div class="critical-error">
+      <img id="critical-error" src="./assets/critical-error.gif">
+      <h2>${err}</h2>
+      <div>
+      `;
+      console.error(err);
+      console.trace();
+    } else {
+      result.innerHTML = `<h1 class="error-message">${err}</h1>`;
     }
   }
 });
